b7cdd20b98839ac8a66f2d5a016c34110ab0ce45
swiftwind0405 <swiftwind0405@gmail.com>
2021-12-12 22:33:15 +0800

    add better solution for 709

--- algorithms/toLowerCase/Solution.js
+++ algorithms/toLowerCase/Solution.js
@@ -3,5 +3,15 @@
  * @return {string}
  */
  var toLowerCase = function(s) {
-    return s.toLowerCase()
+    const sb = [];
+    for (let ch of s) {
+        if (ch.charCodeAt() >= 65 && ch.charCodeAt() <= 90) {
+            // 直接+32也可以
+            // ch = String.fromCharCode(ch.charCodeAt() + 32);
+            ch = String.fromCharCode(ch.charCodeAt() | 32);
+
+        }
+        sb.push(ch);
+    }
+    return sb.join('');
 };
