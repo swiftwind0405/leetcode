18b386c30babc8fb9753b44bce6218172eecf04a
jichangchun <jccweb@163.com>
2022-03-13 20:38:54 +0800

    qs:53

---
+++ algorithms/maxSubArray/maxSubArray.js
@@ -0,0 +1,13 @@
+function maxSubArray (nums) {
+  let ans = nums[0];
+  let sum = 0;
+  for (const num of nums) {
+    if (sum > 0) {
+      sum += num;
+    } else {
+      sum = num;
+    }
+    ans = Math.max(ans, sum);
+  }
+  return ans;
+};
